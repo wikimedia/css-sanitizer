9b47fecc433875eb131950342f49ad361e13a191
Reedy <reedy@wikimedia.org>
2020-04-25 20:51:46 +0100

    Set phan allow_method_param_type_widening = true
    https://github.com/phan/phan/wiki/Phan-Config-Settings#allow_method_param_type_widening
    
    PHP 7.4 turned SeekableIterator::seek( $offset ) into 
    SeekableIterator::seek( int $offset ).
    
    This can be reverted when the library needs PHP >= 7.4 and the param
    type put in place.
    
    Bug: T250934
    Bug: T251043
    Change-Id: Ibbda96887b70e5fd24c68a01a28c55c0c55f4e4e

--- .phan/config.php
+++ .phan/config.php
@@ -22,4 +22,10 @@ $cfg['exclude_analysis_directory_list'] = [ 'vendor/', 'coverage/', 'doc/' ];
 // By default mediawiki-phan-config ignores the 'use of deprecated <foo>' errors.
 // $cfg['suppress_issue_types'][] = '<some phan issue>';
 
+// PHP 7.4 adds int parameter type to SeekableIterator::seek( $offset ). When this library requires
+// PHP >=7.4, add int param type to $offset and remove this conditional. See T250934/T251043
+if ( PHP_VERSION_ID >= 70400 ) {
+	$cfg['allow_method_param_type_widening'] = true;
+}
+
 return $cfg;
