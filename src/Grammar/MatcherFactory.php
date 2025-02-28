73838c79f1c0490e01c1167a1b364051790a567f
Tim Starling <tstarling@wikimedia.org>
2025-02-28 17:14:50 +1100

    Add memoization for MatcherFactory methods that lack it
    Only two zero-argument methods lack caching, they were both recently
    added.
    
    Change-Id: Ibe3421c183331fff28b8adf3ec0b7c65612fd8e0

--- src/Grammar/MatcherFactory.php
+++ src/Grammar/MatcherFactory.php
@@ -266,9 +266,10 @@ class MatcherFactory {
 	 * @return TokenMatcher
 	 */
 	public function colorHex(): TokenMatcher {
-		return new TokenMatcher( Token::T_HASH, static function ( Token $t ) {
-			return preg_match( '/^([0-9a-f]{3}|[0-9a-f]{6})$/i', $t->value() );
-		} );
+		return $this->cache[__METHOD__]
+			??= new TokenMatcher( Token::T_HASH, static function ( Token $t ) {
+				return preg_match( '/^([0-9a-f]{3}|[0-9a-f]{6})$/i', $t->value() );
+			} );
 	}
 
 	/**
@@ -1451,49 +1452,50 @@ class MatcherFactory {
 	 * @return KeywordMatcher
 	 */
 	public function colorWords(): KeywordMatcher {
-		return new KeywordMatcher( [
-			// Basic colors
-			'aqua', 'black', 'blue', 'fuchsia', 'gray', 'green',
-			'lime', 'maroon', 'navy', 'olive', 'purple', 'red',
-			'silver', 'teal', 'white', 'yellow',
-			// Extended colors
-			'aliceblue', 'antiquewhite', 'aquamarine', 'azure',
-			'beige', 'bisque', 'blanchedalmond', 'blueviolet', 'brown',
-			'burlywood', 'cadetblue', 'chartreuse', 'chocolate',
-			'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan',
-			'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray',
-			'darkgreen', 'darkgrey', 'darkkhaki', 'darkmagenta',
-			'darkolivegreen', 'darkorange', 'darkorchid', 'darkred',
-			'darksalmon', 'darkseagreen', 'darkslateblue',
-			'darkslategray', 'darkslategrey', 'darkturquoise',
-			'darkviolet', 'deeppink', 'deepskyblue', 'dimgray',
-			'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite',
-			'forestgreen', 'gainsboro', 'ghostwhite', 'gold',
-			'goldenrod', 'greenyellow', 'grey', 'honeydew', 'hotpink',
-			'indianred', 'indigo', 'ivory', 'khaki', 'lavender',
-			'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue',
-			'lightcoral', 'lightcyan', 'lightgoldenrodyellow',
-			'lightgray', 'lightgreen', 'lightgrey', 'lightpink',
-			'lightsalmon', 'lightseagreen', 'lightskyblue',
-			'lightslategray', 'lightslategrey', 'lightsteelblue',
-			'lightyellow', 'limegreen', 'linen', 'magenta',
-			'mediumaquamarine', 'mediumblue', 'mediumorchid',
-			'mediumpurple', 'mediumseagreen', 'mediumslateblue',
-			'mediumspringgreen', 'mediumturquoise', 'mediumvioletred',
-			'midnightblue', 'mintcream', 'mistyrose', 'moccasin',
-			'navajowhite', 'oldlace', 'olivedrab', 'orange',
-			'orangered', 'orchid', 'palegoldenrod', 'palegreen',
-			'paleturquoise', 'palevioletred', 'papayawhip',
-			'peachpuff', 'peru', 'pink', 'plum', 'powderblue',
-			'rosybrown', 'royalblue', 'saddlebrown', 'salmon',
-			'sandybrown', 'seagreen', 'seashell', 'sienna', 'skyblue',
-			'slateblue', 'slategray', 'slategrey', 'snow',
-			'springgreen', 'steelblue', 'tan', 'thistle', 'tomato',
-			'turquoise', 'violet', 'wheat', 'whitesmoke',
-			'yellowgreen',
-			// Other keywords. Intentionally omitting the deprecated system colors.
-			'transparent', 'currentColor',
-		] );
+		return $this->cache[__METHOD__]
+			??= new KeywordMatcher( [
+				// Basic colors
+				'aqua', 'black', 'blue', 'fuchsia', 'gray', 'green',
+				'lime', 'maroon', 'navy', 'olive', 'purple', 'red',
+				'silver', 'teal', 'white', 'yellow',
+				// Extended colors
+				'aliceblue', 'antiquewhite', 'aquamarine', 'azure',
+				'beige', 'bisque', 'blanchedalmond', 'blueviolet', 'brown',
+				'burlywood', 'cadetblue', 'chartreuse', 'chocolate',
+				'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan',
+				'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray',
+				'darkgreen', 'darkgrey', 'darkkhaki', 'darkmagenta',
+				'darkolivegreen', 'darkorange', 'darkorchid', 'darkred',
+				'darksalmon', 'darkseagreen', 'darkslateblue',
+				'darkslategray', 'darkslategrey', 'darkturquoise',
+				'darkviolet', 'deeppink', 'deepskyblue', 'dimgray',
+				'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite',
+				'forestgreen', 'gainsboro', 'ghostwhite', 'gold',
+				'goldenrod', 'greenyellow', 'grey', 'honeydew', 'hotpink',
+				'indianred', 'indigo', 'ivory', 'khaki', 'lavender',
+				'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue',
+				'lightcoral', 'lightcyan', 'lightgoldenrodyellow',
+				'lightgray', 'lightgreen', 'lightgrey', 'lightpink',
+				'lightsalmon', 'lightseagreen', 'lightskyblue',
+				'lightslategray', 'lightslategrey', 'lightsteelblue',
+				'lightyellow', 'limegreen', 'linen', 'magenta',
+				'mediumaquamarine', 'mediumblue', 'mediumorchid',
+				'mediumpurple', 'mediumseagreen', 'mediumslateblue',
+				'mediumspringgreen', 'mediumturquoise', 'mediumvioletred',
+				'midnightblue', 'mintcream', 'mistyrose', 'moccasin',
+				'navajowhite', 'oldlace', 'olivedrab', 'orange',
+				'orangered', 'orchid', 'palegoldenrod', 'palegreen',
+				'paleturquoise', 'palevioletred', 'papayawhip',
+				'peachpuff', 'peru', 'pink', 'plum', 'powderblue',
+				'rosybrown', 'royalblue', 'saddlebrown', 'salmon',
+				'sandybrown', 'seagreen', 'seashell', 'sienna', 'skyblue',
+				'slateblue', 'slategray', 'slategrey', 'snow',
+				'springgreen', 'steelblue', 'tan', 'thistle', 'tomato',
+				'turquoise', 'violet', 'wheat', 'whitesmoke',
+				'yellowgreen',
+				// Other keywords. Intentionally omitting the deprecated system colors.
+				'transparent', 'currentColor',
+			] );
 	}
 
 	/** @} */
